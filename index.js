731f425b6aec6e0e00634f4c7e0be7225a0e92a2
shubhamgarg21156 <66112766+shubhamgarg21156@users.noreply.github.com>
2021-06-15 21:51:49 +0530

    Added Nav bar and featured products

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const app = express();
 const port = 8000;
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
+const sassMiddleware = require('node-sass-middleware');
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+
+
+app.use(sassMiddleware({
+    src : './assets/scss',
+    dest : './assets/css',
+    debug : true,
+    outputStyle : 'extended',
+    prefix : '/css'
+}));
+
+
 
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
